7807aec4ca46edcb27f5a1fe69f451e69f5ffeb5
divyeshinraj09-commits <divyeshinraj09@gmail.com>
2026-07-01 21:53:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -836,7 +836,8 @@ export default function App() {
 
             <h2 className="text-3xl md:text-5xl font-black text-center uppercase tracking-tighter mb-4 glitch-header select-text">
               DIVYESH FOUND... 
-                   VOTE DIVYESH FOR IT HEAD
+                   
+                                  VOTE DIVYESH FOR IT HEAD
             </h2>
 
             <div className="bg-white border-4 border-black p-4 mb-6 font-mono text-xs md:text-sm space-y-3 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
